f0921365b1f838b33c2632e58337e073d87050f5
tiagop23 <talexandrea@ipvc.pt>
2023-08-02 12:36:28 +0100

    4ºcommit

--- script.js
+++ script.js
@@ -11,6 +11,23 @@ function calculatebmi() {
     document.getElementById('heading').innerHTML = 'Your bmi is';
     document.getElementById('bmi').innerHTML = bmi;
 
+    if (bmi < 18.5) {
+        document.getElementById('message').innerHTML = "Underweight";
+    }
+
+    if (bmi >= 18.5 && bmi < 24.5) {
+        document.getElementById('message').innerHTML = "Normal";
+
+    }
+
+    if (bmi >= 24.5 && bmi < 30) {
+        document.getElementById('message').innerHTML = "Overweight";
+    }
+
+    if (bmi > 30) {
+        document.getElementById('message').innerHTML = "Obesity";
+    }
+
 }
 
 
